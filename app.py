de24e90d76033e8f0dd49a087d1660e80ca948d4
Taylor D. Edmiston <tedmiston@gmail.com>
2020-04-05 21:34:37 -0400

    Refactor into functions for testing

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ from requests_html import HTMLSession
 
 
 @dataclass
-class Stat:
+class Metric:
+    """A metric."""
+
     label: str
     value: str
 
@@ -13,37 +15,53 @@ class Stat:
 
 
 def fetch():
+    """Retrieve html of metrics overview page."""
     url = "https://coronavirus.ohio.gov/wps/portal/gov/covid-19/home"
     session = HTMLSession()
     response = session.get(url)
-    stats_cards = response.html.xpath(
+    return response.html
+
+
+def parse(html):
+    """Scrape metrics tiles from page."""
+    stats_cards = html.xpath(
         '//*[@id="odx-main-content"]/article/section[2]/div', first=True
     )
     tiles = stats_cards.find(".odh-ads__item")[:8]
-    stats = []
-    for idx, tile in enumerate(tiles):
-        value = tile.find(".odh-ads__item-title", first=True).text
-        label = tile.find(".odh-ads__item-summary", first=True).text
-        label = (
-            label.replace("Number of", "")
+    metrics = [
+        Metric(
+            label=tile.find(".odh-ads__item-summary", first=True).text,
+            value=tile.find(".odh-ads__item-title", first=True).text,
+        )
+        for tile in tiles
+    ]
+    return metrics
+
+
+def clean(metrics):
+    """Post-process tile labels."""
+    for i in metrics:
+        i.label = (
+            i.label.replace("Number of", "")
             .replace("in Ohio", "")
             .title()
             .replace("Icu", "ICU")
             .strip()
         )
-        stat = Stat(label=label, value=value)
-        stats.append(stat)
-    return stats
+    return metrics
 
 
-def display(stats):
-    for idx, i in enumerate(stats):
+def display(metrics):
+    """Output metrics tiles to console."""
+    for idx, i in enumerate(metrics):
         print(i, end=("\n\n" if idx in [3, 5] else "\n"))
 
 
 def main():
-    stats = fetch()
-    display(stats)
+    html = fetch()
+    metrics = parse(html)
+    metrics = clean(metrics)
+    display(metrics)
 
 
 if __name__ == "__main__":
