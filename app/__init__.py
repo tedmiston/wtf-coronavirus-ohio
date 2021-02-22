5216a9edb44b0ec05cb49edd06912639d1850bb7
Taylor D. Edmiston <tedmiston@gmail.com>
2021-02-21 20:56:44 -0500

    Run black

--- app/__init__.py
+++ app/__init__.py
@@ -43,11 +43,7 @@ def clean(metrics: List[Metric]) -> List[Metric]:
             .replace("Icu", "ICU")
             .strip()
         )
-        i.value = (
-            i.value
-            .replace("*", "")
-            .replace("-", "–")
-        )
+        i.value = i.value.replace("*", "").replace("-", "–")
     return metrics
 
 
