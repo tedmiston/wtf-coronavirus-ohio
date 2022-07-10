11645e4114132433748142244ad50503d7177b53
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-10 18:44:35 -0400

    Refactor out tile parsing

--- app/__init__.py
+++ app/__init__.py
@@ -15,17 +15,19 @@ def fetch() -> HTML:
     return response.html
 
 
+def _parse_tile(tile) -> Metric:
+    metric = Metric(
+        label=tile.find(".stats-cards__label", first=True).text,
+        value=tile.find(".stats-cards__number", first=True).text,
+    )
+    return metric
+
+
 def parse(html: HTML) -> List[Metric]:
     """Scrape metrics tiles from page."""
     stats_cards = html.find(".stats-cards__container", first=True)
     tiles = stats_cards.find(".stats-cards__item")
-    metrics = [
-        Metric(
-            label=tile.find(".stats-cards__label", first=True).text,
-            value=tile.find(".stats-cards__number", first=True).text,
-        )
-        for tile in tiles
-    ]
+    metrics = [_parse_tile(x) for x in tiles]
     return metrics
 
 
