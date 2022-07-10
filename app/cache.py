e06985ee7999758a671ebfc3435b594ecdd47da7
Taylor D. Edmiston <tedmiston@gmail.com>
2022-07-10 16:17:00 -0400

    Wrap docstring

--- app/cache.py
+++ app/cache.py
@@ -5,7 +5,9 @@ import requests_cache
 
 def setup_cache() -> None:
     """
-    The requests_cache package monkey patches `requests.Session` (used by requests_html) so `requests_cache.install_cache` must be called before `requests_html.HTMLSession` is imported.
+    The requests_cache package monkey patches `requests.Session` (used by requests_html)
+    so `requests_cache.install_cache` must be called before `requests_html.HTMLSession`
+    is imported.
     """
     cache_path_dir = Path.home() / ".wtf-coronavirus-ohio"
     cache_path_dir.mkdir(exist_ok=True)
